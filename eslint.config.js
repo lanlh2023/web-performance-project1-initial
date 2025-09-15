38de10c51b7bfe18ea8b47865b825aae21dca3bb
lanlh2023 <lanlh@zigexn.vn>
2025-09-15 17:19:47 +0700

    update jenkin 29

--- eslint.config.js
+++ eslint.config.js
@@ -1,39 +1,37 @@
 const globals = require("globals");
 
-module.exports = {
-  env: {
-    browser: true,
-    node: true,
-    es2021: true,
-    jest: true
-  },
-  extends: [
-    "eslint:recommended"
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "script"
-  },
-  globals: {
-    ...globals.browser,
-    ...globals.node,
-    ...globals.jest
-  },
-  rules: {
-    "no-unused-vars": "warn",
-    "no-console": "off"
-  },
-  overrides: [
-    {
-      files: ["**/*.test.js", "**/*.spec.js", "**/setupTests.js"],
-      env: {
-        jest: true
-      },
-      plugins: ["jest"],
-      extends: [
-        "eslint:recommended",
-        "plugin:jest/recommended"
-      ]
+module.exports = [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "script",
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "no-undef": "error"
     }
-  ]
-};
+  },
+  {
+    files: ["**/*.test.js", "**/*.spec.js", "**/setupTests.js"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "script",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest
+      }
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "no-undef": "error"
+    }
+  }
+];
