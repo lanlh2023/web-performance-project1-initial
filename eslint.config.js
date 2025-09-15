bb47b5a11376399e72b4954fa84c70163a200fac
lanlh2023 <lanlh@zigexn.vn>
2025-09-15 17:17:44 +0700

    Enable auto-trigger for CI/CD pipeline

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,39 @@
-const js = require("@eslint/js");
 const globals = require("globals");
 
-module.exports = [
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      sourceType: "script",
-      globals: {
-        ...globals.browser,
-        ...globals.node
-      }
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      "no-unused-vars": "warn",
-      "no-console": "off"
-    }
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+    jest: true
   },
-  {
-    files: ["**/*.test.js", "**/*.spec.js", "**/setupTests.js"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      sourceType: "script",
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        ...globals.jest
-      }
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      "no-unused-vars": "warn",
-      "no-console": "off"
+  extends: [
+    "eslint:recommended"
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "script"
+  },
+  globals: {
+    ...globals.browser,
+    ...globals.node,
+    ...globals.jest
+  },
+  rules: {
+    "no-unused-vars": "warn",
+    "no-console": "off"
+  },
+  overrides: [
+    {
+      files: ["**/*.test.js", "**/*.spec.js", "**/setupTests.js"],
+      env: {
+        jest: true
+      },
+      plugins: ["jest"],
+      extends: [
+        "eslint:recommended",
+        "plugin:jest/recommended"
+      ]
     }
-  }
-];
+  ]
+};
